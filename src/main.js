df6ecdac59b62b7ca0e8a2f880c3c912f1dd17f1
TRDEFI <trdefi1985@gmail.com>
2025-07-31 19:21:51 +0300

    Create basic web project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div class="container">
+    <h1>Welcome to Your Web Project</h1>
+    <p>Your development environment is now ready!</p>
+    <div class="card">
+      <p>Start building your application by editing the files in the <code>src</code> directory.</p>
+    </div>
+  </div>
+`
